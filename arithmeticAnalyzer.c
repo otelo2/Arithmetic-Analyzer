b4a6795967f52f37b6b28356ddae68ca3ed3f3d0
Otelo2 <josean2009@live.com.mx>
2020-10-09 12:40:54 -0500

    Read string and show it

--- arithmeticAnalyzer.c
+++ arithmeticAnalyzer.c
@@ -1,9 +1,23 @@
+//Arithmetic analyzer for the Theory of computation course
+//TODO: Add the ID's
+//José Antonio Solís Martínez. 162442
+//María Fernanda Flores Luna. 16XXXX
+//Mauricio Raul Tenorio Guzman. 16XXXX
+//Jorge André Tenorio Guzman. 16XXXX
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
 int main()
 {
-    printf("Hello world");
+    char string[60];
+
+    printf("Input your arithmetic expression: ");
+    fgets(string, 60, stdin);
+
+    printf("Read:\n");
+    puts(string);
+
+
     return 0;
 }
